76d34aa4c856cf7da80202b1bdaab4a566f6b0d0
pierrelardy <mailto:pierre.lardy.90@gmail.com>
2018-01-31 16:01:45 +0100

    test page

--- janus/optionnelles/urls.py
+++ janus/optionnelles/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    path('<int:user_id>/user/', views.user_detail, name='user_detail'),
 ]
--- janus/optionnelles/views.py
+++ janus/optionnelles/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
+from .models import Etudiant
+from django.contrib.auth.models import User
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    user_list = User.objects.all()
+    context = {
+        'user_list': user_list,
+    }
+    return render(request, 'optionnelles/index.html', context)
+
+def user_detail(request, user_id):
+    return HttpResponse("You're looking at question %s." % user_id)
+
 # Create your views here.
