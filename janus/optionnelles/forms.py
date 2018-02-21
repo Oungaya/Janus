9f03f8d02122c920744aeaeb23d39739a81ea199
NinjaOldBoy <alexandre.cadier@gmail.com>
2018-02-21 13:08:31 +0100

    Pourquoi le HEAD ?
    JPP de Git ..

--- janus/optionnelles/forms.py
+++ janus/optionnelles/forms.py
@@ -18,7 +18,6 @@ class InscriptionForm(forms.Form):
     telephone = forms.CharField(widget=forms.TextInput(attrs={'class' : 'mdl-textfield__input'}))
     ajac = forms.BooleanField(required=False,widget=forms.CheckboxInput(attrs={'class' : 'mdl-switch__input'}))
     redoublant = forms.BooleanField(required=False,widget=forms.CheckboxInput(attrs={'class' : 'mdl-switch__input'}))
-<<<<<<< HEAD
 
 class InscriptionProfesseurForm(forms.Form):
     queryStatut = Statut.objects.all()
@@ -27,8 +26,7 @@ class InscriptionProfesseurForm(forms.Form):
     prenom = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class' : 'mdl-textfield__input'}))
     nombre_heures = forms.IntegerField(max_value=99999999999,widget=forms.NumberInput(attrs={'class' : 'mdl-textfield__input'}))
     statut = forms.ModelChoiceField(queryset=queryStatut,widget=forms.Select(attrs={'class' : 'mdl-textfield__input'}))
-=======
->>>>>>> e6c9654cc6f588e8b13f4d95d15e459d70e61414
+
 
 class MpoublieForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class' : 'mdl-textfield__input'}))
