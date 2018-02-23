0126689662c8a64a3cef862639212f1bd7e16856
Oungaya <alypmat@gmail.com>
2018-02-23 14:53:13 +0100

    Reset password operationnel
    dossier /registration contiens tout les templates et fichiers txt pour
    gérer le reset password

--- janus/optionnelles/urls.py
+++ janus/optionnelles/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
+app_name='optionnelles'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
@@ -15,4 +18,30 @@ urlpatterns = [
     path('formulaire_reinitialisation/', views.user_formulaireReinitialisation, name='user_formulaireReinitialisation'),
     path('validation_inscription/', views.admin_ValidationInscription, name='admin_ValidationInscription'),
     path('inscription_professeur/', views.admin_InscriptionProfesseur, name='admin_InscriptionProfesseur'),
+    #url(r'^password_reset/$', auth_views.password_reset, name='password_reset_form'),
+    #url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    #url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #    auth_views.password_reset_confirm, name='password_reset_confirm'),
+    #url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+    #url('^', include('django.contrib.auth.urls')),
+    #path('oubli-mdp/', auth_views.password_reset, name='user_motDePasseOublie'),
+    #path('password_reset/done', auth_views.password_reset_done, name='registration/password_reset_done')
+    #url(r'^password_reset/$', auth_views.password_reset, name='registration/password_reset_form.html'),
+    #url(r'password_reset/done/', auth_views.password_reset_done, name='registration/password_reset_done'), 
+    #url(r'password_reset/', auth_views.password_reset, name='registration/password_reset'),
+    url(r'^password_reset/$', auth_views.password_reset,{'email_template_name':'optionnelles/registration/password_reset_email.html',
+                                                    'template_name':'optionnelles/registration/password_reset_form.html',
+                                                    'subject_template_name':'optionnelles/registration/password_reset_subject.txt',
+                                                    'post_reset_redirect':'optionnelles:password_reset_done',
+                                                    'from_email':'optionnelles@django.com',
+                                                    },name='password_reset'),
+
+    url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'optionnelles/registration/password_reset_done.html'}, name='password_reset_done'),
+
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.password_reset_confirm,
+                                                    {'template_name': 'optionnelles/registration/password_reset_confirm.html',
+                                                    'post_reset_redirect': 'optionnelles:password_reset_complete'},
+                                                    name='password_reset_confirm'),
+
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'optionnelles/registration/password_reset_complete.html'},name='password_reset_complete'),
 ]
