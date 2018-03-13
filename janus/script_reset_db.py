d4541f427ef77b249a940770ffabf614fe527528
NinjaOldBoy <alexandre.cadier@gmail.com>
2018-03-13 23:04:03 +0100

    injection des ue par pole dans le script
    JPP haha

--- janus/script_reset_db.py
+++ janus/script_reset_db.py
@@ -29,7 +29,7 @@ def main():
     INSERT INTO optionnelles_typepole(nom) VALUES (\'Compétences en Ingénierie des SI\');\
     INSERT INTO optionnelles_typepole(nom) VALUES (\'Compétences en Informatique\');\
     INSERT INTO optionnelles_typepole(nom) VALUES (\'Compétences en Gestion\');\
-    INSERT INTO optionnelles_typepole(nom) VALUES (\'Domaine d''application\');\
+    INSERT INTO optionnelles_typepole(nom) VALUES (\'Domaine d application\');\
     INSERT INTO optionnelles_typepole(nom) VALUES (\'Compétences Transverses\');\
     INSERT INTO optionnelles_typepole(nom) VALUES (\'Professionnalisation\');\
     INSERT INTO optionnelles_typepole(nom) VALUES (\'eSanté\');\
@@ -58,6 +58,8 @@ def main():
     INSERT INTO optionnelles_pole(nom, nombre_options, parcours_id, typePole_id) VALUES (\'M2 SIS Compétences Transverses\', \'0\', \'5\', \'5\');\
     INSERT INTO optionnelles_pole(nom, nombre_options, parcours_id, typePole_id) VALUES (\'M2 SIS eSanté\', \'0\', \'5\', \'7\');\
     INSERT INTO optionnelles_pole(nom, nombre_options, parcours_id, typePole_id) VALUES (\'M2 SIS Professionnalisation\', \'0\', \'5\', \'6\');\
+    INSERT INTO optionnelles_pole(nom, nombre_options, parcours_id, typePole_id) VALUES (\'M2 INE Compétence en Ingénierie des SI\', \'0\', \'7\', \'1\');\
+    INSERT INTO optionnelles_pole(nom, nombre_options, parcours_id, typePole_id) VALUES (\'M2 INE Professionnalisation\', \'27\', \'7\', \'6\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Conduite de projet\', \'ISI_02\', \'0\', \'0\', \'8\', \'40\', \'3\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Ingénierie du logiciel\', \'ISI_05\', \'0\', \'0\', \'8\', \'22\', \'2\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Architecture client serveur\', \'ISI_01\', \'0\', \'2\', \'8\', \'22\', \'2\');\
@@ -98,7 +100,7 @@ def main():
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Architecture des SI 2\', \'INFO_18\', \'0\', \'0\', \'0\', \'30\', \'1\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'ASI Mobile 2\', \'INFO_19\', \'0\', \'0\', \'0\', \'30\', \'1\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Web Design\', \'INFO_31\', \'0\', \'0\', \'8\', \'22\', \'1\');\
-    INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Stratégie d''entreprise\', \'GEO_08\', \'0\', \'0\', \'8\', \'22\', \'2\');\
+    INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Stratégie d entreprise\', \'GEO_08\', \'0\', \'0\', \'8\', \'22\', \'2\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Théories des organisations\', \'GEO_09\', \'0\', \'0\', \'8\', \'22\', \'1\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Anglais\', \'TRANS_08\', \'0\', \'0\', \'0\', \'30\', \'2\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Environnement socio-économique\', \'TRANS_10\', \'0\', \'0\', \'0\', \'30\', \'2\');\
@@ -121,6 +123,133 @@ def main():
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Electrophysiologie\', \'SANTE_05\', \'0\', \'0\', \'0\', \'30\', \'2\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Instrumentation biomédicale 2\', \'SANTE_06\', \'0\', \'0\', \'0\', \'30\', \'1\');\
     INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Télémédecine\', \'SANTE_07\', \'0\', \'0\', \'0\', \'30\', \'2\');\
+    INSERT INTO optionnelles_ue(nom, num_UE, code_apoge, nombre_groupes, nombre_heures_CM, nombre_heures_TD, semestre_id) VALUES (\'Etude de cas création d entreprise\', \'PRO_02\', \'0\', \'0\', \'0\', \'0\', \'3\');\
+    INSERT INTO optionnelles_ue_par_pole VALUES (1, 0, 1, 1);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (2, 0, 1, 2);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (3, 1, 1, 3);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (4, 1, 1, 4);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (5, 1, 1, 5);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (6, 0, 2, 6);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (7, 0, 2, 7);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (8, 0, 2, 8);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (9, 1, 2, 9);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (10, 1, 2, 10);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (11, 1, 2, 11);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (12, 1, 2, 12);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (13, 1, 2, 13);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (14, 1, 2, 14);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (15, 0, 3, 15);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (16, 0, 3, 16);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (17, 0, 3, 17);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (18, 0, 3, 18);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (19, 1, 3, 19);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (20, 1, 3, 20);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (21, 1, 3, 21);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (22, 0, 4, 22);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (23, 0, 4, 23);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (24, 1, 4, 24);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (25, 1, 4, 25);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (26, 1, 4, 26);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (27, 1, 4, 27);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (28, 1, 4, 28);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (29, 0, 5, 29);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (30, 0, 6, 30);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (31, 0, 6, 31);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (32, 0, 6, 32);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (33, 0, 6, 33);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (34, 0, 6, 34);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (35, 1, 6, 35);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (36, 1, 6, 36);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (37, 1, 6, 37);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (38, 0, 7, 38);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (39, 1, 7, 39);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (40, 1, 7, 40);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (41, 0, 8, 41);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (42, 0, 8, 42);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (43, 0, 9, 43);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (44, 0, 9, 44);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (45, 0, 10, 45);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (46, 0, 10, 46);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (47, 0, 11, 33);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (49, 0, 11, 47);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (50, 0, 12, 48);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (51, 0, 12, 49);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (52, 0, 12, 38);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (53, 0, 12, 50);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (54, 0, 12, 51);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (55, 1, 12, 52);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (56, 1, 12, 53);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (57, 0, 13, 41);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (58, 0, 13, 42);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (59, 0, 14, 43);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (60, 0, 14, 54);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (61, 0, 14, 44);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (62, 0, 15, 45);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (63, 0, 15, 46);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (64, 0, 16, 32);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (65, 0, 16, 33);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (66, 1, 16, 36);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (67, 1, 16, 37);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (68, 0, 17, 38);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (69, 0, 17, 55);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (70, 0, 17, 52);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (71, 0, 17, 56);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (72, 1, 17, 39);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (73, 1, 17, 40);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (74, 1, 17, 53);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (75, 0, 18, 41);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (76, 0, 19, 43);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (77, 0, 19, 54);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (78, 0, 19, 44);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (79, 0, 20, 45);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (80, 0, 20, 46);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (81, 0, 21, 33);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (82, 0, 21, 35);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (83, 0, 22, 57);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (84, 0, 22, 52);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (85, 0, 22, 58);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (86, 0, 22, 56);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (87, 1, 22, 59);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (88, 1, 22, 60);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (89, 0, 23, 54);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (90, 0, 23, 44);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (91, 0, 23, 43);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (92, 0, 24, 61);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (93, 0, 24, 62);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (94, 0, 24, 63);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (95, 0, 25, 45);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (96, 0, 25, 46);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (97, 0, 26, 33);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (98, 0, 26, 41);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (99, 0, 27, 64);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (100, 0, 27, 46);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (101, 1, 27, 42);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (102, 1, 27, 48);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (103, 1, 27, 49);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (104, 1, 27, 38);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (105, 1, 27, 39);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (106, 1, 27, 55);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (107, 1, 27, 57);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (108, 1, 27, 59);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (109, 1, 27, 52);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (110, 1, 27, 58);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (111, 1, 27, 56);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (112, 1, 27, 50);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (113, 1, 27, 60);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (114, 1, 27, 51);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (115, 1, 27, 40);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (116, 1, 27, 53);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (117, 1, 27, 31);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (118, 1, 27, 32);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (119, 1, 27, 35);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (120, 1, 27, 36);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (121, 1, 27, 47);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (122, 1, 27, 61);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (123, 1, 27, 62);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (124, 1, 27, 63);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (125, 1, 27, 43);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (126, 1, 27, 54);\
+    INSERT INTO optionnelles_ue_par_pole VALUES (127, 1, 27, 44);\
     "', shell=True)
     subprocess.call('python3 manage.py createsuperuser', shell=True)
 
