e630c3bcb41e0d224d147b515b625d77a43f8c55
Oungaya <alypmat@gmail.com>
2018-03-26 18:06:33 +0200

    les modifs de optionnelles sont pas prises en compte, je sais pas pourquoi

--- janus/janus/settings.py
+++ janus/janus/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = ['janusoptionnelles.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'optionnelles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'optionnelles',
+
 ]
 
 EMAIL_HOST = 'smtp.sendgrid.net'
