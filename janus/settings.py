d1dd696ce356faad0e8646553f6ab2a994914fd2
Oungaya <alypmat@gmail.com>
2018-05-06 16:51:24 +0200

    essai sans optionnelles

--- janus/settings.py
+++ janus/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['janusoptions.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'optionnelles',
+    #'optionnelles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
