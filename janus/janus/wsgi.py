8eab2157df5b57695e960ce3f7d05551529416a5
Oungaya <alypmat@gmail.com>
2018-05-12 14:58:30 +0200

    hasta la vista

--- janus/janus/wsgi.py
+++ janus/janus/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/apps/janus'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'janus.settings'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "janus.settings")
-
-application = get_wsgi_application()
