ad2918369355843af5c43351354a295387140af8
Oungaya <alypmat@gmail.com>
2018-05-15 20:02:11 +0200

    projet restructuré

--- janus/urls.py
+++ janus/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.conf import settings
+from optionnelles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index),
     url(r'^options/', include('optionnelles.urls'), name='optionnelles'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
